ce174472ed009a8876ecc5abefcf0804799a4ac1
yhy7805 <yhy7805@gmail.com>
2023-10-07 18:52:32 +0900

    최종 코드 정리

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+//전역변수 정보값
+//외부데이터 fetching시 준비사항, api-key발급, 쿼리값
+//api-key,가져올 데이터 갯수, 플레이리스트 아이디
+//위의 쿼리값들을 조합해서 최종 데이터 요청 url완성
 const frame = document.querySelector('section');
 const api_key = 'AIzaSyDqvoC8xc_hHGMQLtNaUT7GBsbUTB_uHyo';
 const baseURL = 'https://www.googleapis.com/youtube/v3/playlistItems';
@@ -7,17 +11,27 @@ const resultURL = `${baseURL}?key=${api_key}&part=snippet&playlistId=${pid}&maxR
 const tit_len = 30;
 const desc_len = 180;
 
+//동적으로 생성되는 요소에는 무조건 window에 이벤트 위임을 걸고
 window.addEventListener('click', (e) => {
+	//선택자요소에 따라 다른 다른 함수를 호출
+	//썸네일 클릭시 팝업 호출
 	if (e.target.nodeName === 'IMG') createPop(e.target.getAttribute('data-vid'));
+	//닫기버튼 클릭시 팝업 제거
 	if (e.target.className === 'close') removePop();
 });
+
+//브라우저 로딩시 fetch함수 호출
 fetch(resultURL)
+	//promise로 넘어온 데이터를 동기적으로 받아서 json형태로 parsing한 다음에
 	.then((data) => data.json())
 	.then((json) => {
 		console.log(json.items);
 		let tags = '';
 
+		//parsing된 배열데이터를 반복돌면서
+		//동적으로 목록 생성
 		json.items.map((data) => {
+			//글자값이 길어서 글자짜르기로 가공처리
 			let desc = data.snippet.description;
 			desc.length > desc_len ? (desc = desc.substr(0, desc_len) + '...') : desc;
 
@@ -46,6 +60,7 @@ fetch(resultURL)
 		frame.innerHTML = tags;
 	});
 
+//이벤트위임으로 썸네일 클릭시 동적으로 팝업호출시 영상출력 함수
 function createPop(id) {
 	console.log(id);
 	const aside = document.createElement('aside');
@@ -59,6 +74,7 @@ function createPop(id) {
 	document.body.style.overflow = 'hidden';
 }
 
+//이벤트위임으로 닫기 버튼 클릭시 팝업 제거하는 함수
 function removePop() {
 	const pop = document.querySelector('aside');
 	pop.remove();
